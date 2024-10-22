67803a094304a7a714ce7368831d8d9430c5cd9c
青城 <118874466+qingcheng2022@users.noreply.github.com>
2024-10-22 22:58:36 +0800

    Update 乐园币助力.py

--- 乐园币助力.py
+++ 乐园币助力.py
@@ -141,7 +141,7 @@ class LYB:
 
         try:
             r = requests.post(
-                "http://192.168.1.124:1777/api/getXSign",
+                "http://192.168.1.16:999/api/getXSign",
                 json=body
             )
             r.raise_for_status()
