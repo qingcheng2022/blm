208c0f8b9a485f7878925a92351af7d086f70965
青城 <118874466+qingcheng2022@users.noreply.github.com>
2024-10-22 23:58:38 +0800

    Update and rename ele_femf.js to 福尔魔方.js

--- 福尔魔方.js
+++ 福尔魔方.js
@@ -1,10 +1,9 @@
 /**
- * @kolikow
  * 变量：elmck: 必填，账号cookie
  * cron: 11 0,9,5,23,19 * * *
  */
 
-const $ = new Env('饿了么福尔魔方');
+const $ = new Env('饱了么福尔魔方');
 const qs = require("qs");
 const request = require("request");
 
